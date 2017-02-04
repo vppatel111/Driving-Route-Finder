b8d398402a6da0fce38ca41f246c1ed0b92bc6fd
Rizwan Qureshi <rizwan@ualberta.ca>
2017-02-04 11:23:19 -0700

    Finished version of dijkstra algorithm (not tested yet)

--- dijkstra_algorithm.py
+++ dijkstra_algorithm.py
@@ -15,7 +15,7 @@ def least_cost_path(graph, start, dest, cost):
         cost:   A function, taking the two vertices of an edge as
                 parameters and returning the cost of the edge. For its
                 interface, see the definition of cost_distance.
-                
+
     Returns:
         list:   A potentially empty list (if no path can be found) of
                 the vertices in the graph. If there was a path, the first
@@ -23,3 +23,17 @@ def least_cost_path(graph, start, dest, cost):
                 Any two consecutive vertices correspond to some
                 edge in graph.
     """
+    todolist = queue.deque([start])  # todolist also stores "from where"
+    # reached is a dictionary containing the tuple:
+    # (other edge element, cost of edge, list of the path to reach key vertex)
+    reached = {start: (start, 0, [start])}
+    while todolist:
+        v = todolist.popleft()
+        for w in g.neighbours(v): # for each neighbhour to v
+            edge_cost = cost((v,w))
+            if w not in reached:
+                reached[w] = (v, edge_cost, reached[v][2].append(w))  # w has just been discovered
+                todolist.append(w) # find neighbours to w
+            elif (e for i, e, l in reached[w]) > edge_cost: #elif better path cost
+                reached[w] = (v, edge_cost, reached[v][2].append(w))
+    return reached[dest][2]
