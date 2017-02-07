db126e4ff5fb877fdaa9ca1e0d7bc6596187a0c3
cmput274 <cmput274@ualberta.ca>
2017-02-06 19:34:59 -0700

    Finished user input

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import csv
+import sys
 
 
 def read_graph(file_name, verticesInfo, edgesInfo):
@@ -39,6 +40,37 @@ def read_graph(file_name, verticesInfo, edgesInfo):
     return (g, verticesInfo, edgesInfo)
 
 
+# Built upon the concept introduced by pathDist
+def pathDist(v, u):
+    ''' Returns length of path
+
+    Args:
+        v, u (tuple): (latitude, longitude)
+    Returns:
+        dist = distance of path
+    '''
+
+    # print("pathDist", v, u)
+    return((v[1]-u[1])*(v[1]-u[1])+(v[0]-u[0])*(v[0]-u[0]))
+
+
+# Further testing required
+def findVertex(latitude, longitude, vertices):
+    ''' Returns closes vertex to given latitude and longitude
+
+    Args:
+        latitude (int), longitude (int): latitude and longitude of vertex
+        verticesInfo (dict): Contains info: vertex: (latitude, longitude)}
+    Returns:
+        v = closest vertex
+    '''
+
+    vertex = (latitude, longitude)
+    # print("vertex", vertex)
+    return min(vertices, key=lambda vertex1=vertices.get:
+               pathDist(vertices[vertex1], vertex))
+
+
 file_name = "edmonton-roads-2.0.1.txt"
 verticesInfo = {}  # {vertex: (latitude, longitude)}
 edgesInfo = {}  # {(vertex1, vertex2): (street_name)}
@@ -48,3 +80,33 @@ g, verticesInfo, edgesInfo = read_graph(file_name, verticesInfo, edgesInfo)
 # print(verticesInfo)
 # print(edgesInfo)
 # print(g.vertices())
+
+outputBuffer = []  # Buffer for outputting to std.out
+
+# NOTE: Test code. REMOVE LATER
+# supermin = min(verticesInfo, key=verticesInfo.get)
+# print(supermin)
+
+for line in sys.stdin:
+    line = line.split()
+    # print(line)
+
+    # Skip if line is empty
+    if line == "":
+        continue
+
+    # Arduino requests next data in buffer.
+    if line[0] == "A":
+        print(outputBuffer.pop(0))
+
+    # Arduino sends request for map.
+    if line[0] == "R":
+        startLat = int(float(line[1]) * 1000000)
+        startLon = int(float(line[2]) * 1000000)
+        endLat = int(float(line[3]) * 1000000)
+        endLon = int(float(line[4]) * 1000000)
+        start = findVertex(startLat, startLon, verticesInfo)
+        end = findVertex(endLat, endLon, verticesInfo)
+        print("start and end", start, end)
+
+    # print(startLat, startLon, endLat, endLon)
