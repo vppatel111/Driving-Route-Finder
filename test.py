21ebef4b1369eaa0e51d78b7e1907ed399041792
Rizwan Qureshi <rizwan@ualberta.ca>
2017-02-11 10:08:33 -0700

    just incase

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ This file is strictly for the sake of testing
 Run using the following command in the command line (make sure you are in the right directory!):
             python3 test.py
 '''
-import adjacencygraph
+from adjacencygraph import AdjacencyGraph as Graph
 from time import time
 from dijkstra_algorithm import least_cost_path
 #from cost_distance import cost_distance
@@ -23,13 +23,28 @@ def cost_distance(u, v):
     (x1, y1) = verticesInfo[u]
     (x2, y2) = verticesInfo[v]
     return (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)
-
+'''
+test_case:
+1: v1 = 29577354, v2 = 1455345886
+2: v1 = 30198552, v2 = 225181634
+'''
 fileName = "edmonton-roads-2.0.1.txt"
 verticesInfo = {}
 edgesInfo = {}
 t = time()
-g, verticesInfo, edgesInfo = read_graph(fileName, verticesInfo, edgesInfo)
+#g, verticesInfo, edgesInfo = read_graph(fileName, verticesInfo, edgesInfo)
 print(time()-t)
-shortest_path = least_cost_path(g, 29577354, 1455345886, cost_distance)
+t = time()
+#shortest_path = least_cost_path(g, 30198552, 1455345886, cost_distance)
+s = {1,2,3,4,5,6}
+l = [(1,2), (1,3), (1,6), (2,1),
+(2,3), (2,4), (3,1), (3,2), (3,4), (3,6), (4,2), (4,3),
+(4,5), (5,4), (5,6), (6,1), (6,3), (6,5)]
+weights = {(1,2): 7, (1,3):9, (1,6):14, (2,1):7, (2,3):10,
+(2,4):15, (3,1):9, (3,2):10, (3,4):11, (3,6):2,
+(4,2):15, (4,3):11, (4,5):6, (5,4):6, (5,6):9, (6,1):14,
+(6,3):2, (6,5):9}
+cost = lambda e: weights.get(e, float("inf"))
+print(least_cost_path(graph, 1, 5, cost))
 print(time()-t)
 print(shortest_path)
